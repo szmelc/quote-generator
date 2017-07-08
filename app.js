3ec0a5b136b92e40877b1126242f116c4ac75301
Lukas Szmelc <lszmelc@gmail.com>
2017-07-08 15:26:17 +0200

    http > https

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+  $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
     var quote = json.quoteText
     $('.twitter-share-button').attr('href', "https://twitter.com/intent/tweet?text=" + quote)
     var quote = "<h1>" + json.quoteText + "</h1>";
